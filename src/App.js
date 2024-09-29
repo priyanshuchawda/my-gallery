51fb0f004afba811eb8b4ec787bf1b572a9c71eb
Priyanshu Chawda <hellopriyanshu4@gmail.com>
2024-09-29 09:46:31 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-   // src/App.js
+
    import React from 'react';
-   import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+   import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
    import Gallery from './components/Gallery';
 
    function App() {
        return (
            <Router>
-               <Switch>
-                   <Route path="/" component={Gallery} />
-               </Switch>
+               <Routes>
+                   <Route path="/" element={<Gallery />} />
+               </Routes>
            </Router>
        );
    }
